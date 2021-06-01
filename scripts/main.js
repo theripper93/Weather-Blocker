317085a6be5b9161779de81d86a1381dff2ab03d
theripper93 <tsplab@gmail.com>
2021-06-01 13:45:47 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,37 @@
-Hooks.on("canvasReady",()=>{
-  refreshWheatherBlockingMask();
-})
+let _occludedTilesState = "";
 
-Hooks.on("createDrawing",()=>{
+Hooks.on("canvasReady", () => {
   refreshWheatherBlockingMask();
-})
+});
 
-Hooks.on("updateDrawing",()=>{
+Hooks.on("createDrawing", () => {
   refreshWheatherBlockingMask();
-})
+});
 
-Hooks.on("deleteDrawing",()=>{
+Hooks.on("updateDrawing", () => {
   refreshWheatherBlockingMask();
-})
+});
 
-function refreshWheatherBlockingMask() {
+Hooks.on("deleteDrawing", () => {
+  refreshWheatherBlockingMask();
+});
+
+function refreshWheatherBlockingMask(sight = false) {
+  if (sight) {
+    let _oldOccludedTilesState = _occludedTilesState;
+    _occludedTilesState = "";
+    canvas.foreground.placeables.forEach((t) => {
+      if(t.roomPoly)_occludedTilesState += t.roomPoly.contains(canvas.tokens.controlled[0].center.x,canvas.tokens.controlled[0].center.y) ? "t" : "f";
+    });
+    if (_occludedTilesState == _oldOccludedTilesState) return;
+  }
   let g = new PIXI.Graphics();
-  g.beginFill(0x000000).drawRect(0, 0, canvas.scene.dimensions.width, canvas.scene.dimensions.height);
+  g.beginFill(0x000000).drawRect(
+    0,
+    0,
+    canvas.scene.dimensions.width,
+    canvas.scene.dimensions.height
+  );
   function adjustPolygonPoints(drawing) {
     let globalPoints = [];
     drawing.data.points.forEach((p) => {
@@ -27,20 +42,28 @@ function refreshWheatherBlockingMask() {
   let weatherBlockDrawings = canvas.drawings.placeables.filter(
     (d) => d.data.text == "blockWeather" && d.data.points.length != 0
   );
-  weatherBlockDrawings.forEach(drawing => {
+  weatherBlockDrawings.forEach((drawing) => {
     let p = new PIXI.Polygon(adjustPolygonPoints(drawing));
     g.beginHole().drawPolygon(p).endHole();
   });
-  canvas.foreground.placeables.forEach((t)=>{
-    if(t.roomPoly && (t.occluded || t.alpha == 0 || (canvas.tokens.controlled[0] && t.roomPoly.contains(canvas.tokens.controlled[0].center.x,canvas.tokens.controlled[0].center.y)))){
+  canvas.foreground.placeables.forEach((t) => {
+    if (
+      t.roomPoly &&
+      (t.occluded ||
+        t.alpha == 0 ||
+        (canvas.tokens.controlled[0] &&
+          t.roomPoly.contains(
+            canvas.tokens.controlled[0].center.x,
+            canvas.tokens.controlled[0].center.y
+          )))
+    ) {
       g.beginHole().drawPolygon(t.roomPoly).endHole();
     }
-  })
+  });
   canvas.effects.mask = g;
   g.name = "weatherBlock";
   canvas.effects.children.forEach((c) => {
-    if (c.name == "weatherBlock")
-      c.destroy();
+    if (c.name == "weatherBlock") c.destroy();
   });
   canvas.effects.addChild(g);
 }
